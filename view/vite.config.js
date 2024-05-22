80ee8eb3f6db59733134b7f1d56a31a6ed7f7d21
Ertanic <black.ikra.14@gmail.com>
2024-05-22 16:16:30 +0300

    Build and optimize the html file added

---
+++ view/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite"
+import ViteMinifyPlugin from "vite-plugin-minify"
+import { viteSingleFile } from "vite-plugin-singlefile"
+export default defineConfig({
+    plugins: [viteSingleFile({
+        inlinePattern: ["*.js", "*.css"],
+    }), ViteMinifyPlugin()],
+    build: {
+        minify: "terser"
+    }
+})
