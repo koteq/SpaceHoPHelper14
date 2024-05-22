4944335045242e15bce16fff507cb3f60be51c60
Ertanic <black.ikra.14@gmail.com>
2024-05-22 18:45:44 +0300

    The build script has been updated

--- build.rs
+++ build.rs
@@ -1,9 +1,14 @@
-use std::env;
+use std::{env, process::Command};
 
 use winresource::WindowsResource;
 
 fn main() {
     if env::var_os("CARGO_CFG_WINDOWS").is_some() {
+        Command::new("cmd").arg("/C")
+            .args(&["yarn", "run", "build"])
+            .current_dir("view")
+            .status().expect("Failed to execute command");
+
         WindowsResource::new()
             // This path can be absolute, or relative to your crate root.
             .set_icon("assets/icons/icon.ico")
