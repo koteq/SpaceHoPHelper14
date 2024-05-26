40c1dc5d641032da608f6c7395319a1ed9204a66
Ertanic <black.ikra.14@gmail.com>
2024-05-26 13:29:02 +0300

    The project has been moved to Tauri

--- build.rs
+++ build.rs
@@ -1,18 +1,3 @@
-use std::{env, process::Command};
-
-use winresource::WindowsResource;
-
 fn main() {
-    if env::var_os("CARGO_CFG_WINDOWS").is_some() {
-        Command::new("cmd").arg("/C")
-            .args(&["yarn", "run", "build"])
-            .current_dir("view")
-            .status().expect("Failed to execute command");
-
-        WindowsResource::new()
-            // This path can be absolute, or relative to your crate root.
-            .set_icon("assets/icons/icon.ico")
-            .compile()
-            .unwrap();
-    }
+    tauri_build::build()
 }
