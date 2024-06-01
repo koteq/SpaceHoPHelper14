7bec202a305f7cb163fedbd878108c9f76455372
Ertanic <black.ikra.14@gmail.com>
2024-06-01 19:12:26 +0300

    Added a command to get a link to a repository

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod commands;
+use commands::get_repository;
+
+use tauri::generate_handler;
+
 fn main() {
     tauri::Builder::default()
+      .invoke_handler(generate_handler![get_repository])
       .run(tauri::generate_context!())
       .expect("error while running tauri application");
 }
