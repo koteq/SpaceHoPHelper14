1c50972e1d81e70b48e484ed95d62a69111ad7d2
Ertanic <black.ikra.14@gmail.com>
2024-06-02 01:08:56 +0300

    Code not available on the web is wrapped with a checker for removal at build time

--- view/updates.js
+++ view/updates.js
@@ -3,31 +3,33 @@ import { relaunch } from "@tauri-apps/api/process";
 import { checkUpdate, installUpdate, onUpdaterEvent } from "@tauri-apps/api/updater";
 import { Modal } from 'bootstrap';
 
-invoke('get_repository').then(res => {
-    document.querySelectorAll('.repo-link').forEach(link => {
-        link.setAttribute('href', res); 
-    });
-})
-
-const unlisten = await onUpdaterEvent(({ error, status }) => {
-    console.log('Updater event', error, status)
-})
-
-try {
-    const update = await checkUpdate();
-
-    if (update.shouldUpdate) {
-        const modal = document.querySelector('#updateModal .modal-body > p');
-        modal.textContent = modal.textContent.replace('{newVersion}', update.manifest.version);
-        new Modal(document.getElementById('updateModal')).show();
+if (!import.meta.env.VITE_WEB) {
+    invoke('get_repository').then(res => {
+        document.querySelectorAll('.repo-link').forEach(link => {
+            link.setAttribute('href', res); 
+        });
+    })
+    
+    const unlisten = await onUpdaterEvent(({ error, status }) => {
+        console.log('Updater event', error, status)
+    })
+    
+    try {
+        const update = await checkUpdate();
+    
+        if (update.shouldUpdate) {
+            const modal = document.querySelector('#updateModal .modal-body > p');
+            modal.textContent = modal.textContent.replace('{newVersion}', update.manifest.version);
+            new Modal(document.getElementById('updateModal')).show();
+        }
+    } catch (err) {
+        console.error(err)
     }
-} catch (err) {
-    console.error(err)
+    
+    document.querySelector('#update-button').addEventListener('click', async () => {
+        await installUpdate();
+        await relaunch();
+    });
+    
+    unlisten();   
 }
-
-document.querySelector('#update-button').addEventListener('click', async () => {
-    await installUpdate();
-    await relaunch();
-});
-
-unlisten();
